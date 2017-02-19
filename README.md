ef24aa8415768d427496d6fd9cbd47c2ea2bff73
AmishNazi <holmesjoshuar@gmail.com>
2017-02-19 18:59:12 -0500

    Initial Commit History Reset.
    Rogan Board AlienPoop Skin

---
+++ README.md
@@ -0,0 +1,18 @@
+# AlienPoop
+vBulletin Skin Mod
+
+## Requires
+### Chrome
+[Tampermonkey For Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en)
+### Firefox
+[Greasemonkey For Firefox](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)
+
+###### Current Caveats:
+* Multiquote is broken ATM
+* Tablets with 1024x768 or higher resolution should work pretty well. Firefox, with the USI addon.
+* Mobile is experimental.  Works in Firefox with USI add on installed. Threadlist is clunky, but show thread is nice.  Reccomend turn post per page down to like 20 if you use this one.
+* Haven't tested on Apple versions of Firefox and Chrome.  If there are errors and you have one of those devices let me know.
+* Background image can be edited in the script.  It's located at the top. 
+    * `// @resource    tits https://raw.githubusercontent.com/AmishNazi/AlienPoop/master/Rogan_Board/o2589538.jpg`
+* Going to leave the default image of the sky fire as a notification that the script has been updated.
+* You can either let greasemonkey / tampermonkey handle the updates, or you can use git.  If you need it [GIT GUI](http://home.git.com) is pretty legit for simple stuff like this.
---
+++ Rogan_Board/Rogan_Board.user.js
@@ -0,0 +1,47 @@
+﻿// ==UserScript==
+// @name        Rogan Board
+// @namespace   http://forums.joerogan.net/
+// @description Thread mod.
+// @version     1
+// @grant       GM_getResourceURL
+// @grant       GM_getResourceText
+// @grant       GM_addStyle
+// @include     http://forums.joerogan.net/*
+// @require     https://code.jquery.com/jquery-3.1.1.min.js
+// @require     http://js.nicedit.com/nicEdit-latest.js
+// @resource    rogan https://raw.githubusercontent.com/AmishNazi/AlienPoop/master/Rogan_Board/rogan.css
+// @run-at      document-start
+// ==/UserScript==
+
+GM_addStyle(GM_getResourceText('rogan'));
+$(document).ready(function () {
+    $.expr[':'].textEquals = function (a, i, m) {
+    return $(a).text().match('^' + m[3] + '$');
+  };
+    var tits  = 'https://nik.bot.nu/o1642559.jpg';
+    var panel = 'https://raw.githubusercontent.com/AmishNazi/AlienPoop/master/Rogan_Board/panel.png';
+  $('#vbulletin_css').remove();
+  $('#vB_Editor_QR_textarea').removeAttr('cols');
+  $('#vB_Editor_QR_textarea').removeAttr('rows');
+  $('.page').css('background-image', 'url("' + panel + '")');
+  $('#vB_Editor_QR').css('background-image', 'url("' + panel + '")');
+  $('html').css('background-image', 'url("' + tits + '")');
+  $('.posts').removeAttr('style');
+  $('.bigusername').removeAttr('style');
+  $('.bigusername').parent().css('font-size', '18px');
+  $('tbody[id="qr_error_tbody"]').toggle();
+  $('tbody[id="collapseobj_forumrules"]').toggle();
+  $('div:textEquals("Quote:")').next().css('border', 'dashed');
+  $('div:textEquals("Quote:")').next().css('border-width', '1.25px');
+  $('div:textEquals("Quote:")').next().css('margin-bottom', '16px');
+  $('img[title="Reply With Quote"]').replaceWith('<button class="fa fa-mail-reply"></button>');
+  $('img[title="Multi-Quote This Message"]').replaceWith('<button class="fa fa-quote-right"></button>');
+  $('img[title="Quick reply to this message"]').replaceWith('<button class="fa fa-rocket"></button>');
+  $('img[title="Edit/Delete Message"]').replaceWith('<button class="fa fa-pencil-square-o"></button>');
+  $('a:any-link').css('color', '#ACACAC');
+  $('html').css('visibility', 'visible');
+  $('tbody#qr_error_tbody').detach();
+  $(".thead:contains('PM')").wrapInner("<div class=date></div>");
+  $(".thead:contains('AM')").wrapInner("<div class=date></div>");
+  $('map[name="vb_headmap"]').remove();
+});
